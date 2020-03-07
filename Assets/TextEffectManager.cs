5ead9face569b53e2a13c36089084de70a93464a
Yukii <yukiizzzx@gmail.com>
2020-03-07 18:18:59 -0500

    Regex.match?

--- Assets/TextEffectManager.cs
+++ Assets/TextEffectManager.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 using UnityEngine;
 
 public class TextEffectManager : MonoBehaviour
@@ -9,20 +11,33 @@ public class TextEffectManager : MonoBehaviour
     {
         
     }
-    public string a = "she starts <tag1>to behavior in </tag1>a pretty wired <tag2>way. However, other people are</tag2> apparently not realizing this.";
+    public string a = "<she starts <tag1>to behavior in </tag1>a pretty wired <tag2>way. However, other people are</tag2> apparently not realizing this.";
     // Start is called before the first frame update
+    public TextEffectManager()
+    {
+    }
+
     void Start()
     {
-        
+        var str = "";
+        GetTags(a, out str);
     }
 
     // Update is called once per frame
     public List<Tags> GetTags(string sentence, out string cleanSentence)
     {
-        
+        var wordList = sentence.Split("<"[0],">"[0]).ToList();
+        var match = Regex.Matches(a, "[<->]");
+        for (int i = 1; i < wordList.Count; i++)
+        {
+            //wordList[i] = "<" + wordList[i];
+        }
+        _PrintString(match);
+        cleanSentence = "";
+        return new List<Tags>();
     }
 
-    private void _PrintString(List<string> b)
+    private void _PrintString(IEnumerable b)
     {
         foreach (var sentence in b)
         {
@@ -30,7 +45,7 @@ public class TextEffectManager : MonoBehaviour
         }
     }
 
-    private string _PutStringBack(List<string> b)
+    private string _PutStringBack(IEnumerable b)
     {
         var str = "";
         foreach (var sentence in b)
